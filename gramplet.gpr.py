0a8dc8dc6bd543a38794688d31d1c327a6fdefc0
Emyoulation or BAMaustin <69127217+emyoulation@users.noreply.github.com>
2023-08-08 10:56:06 -0500

    Update gramplet.gpr.py
    change maintainers and maintainers_email to be lists.  (Guessing at eMail format.  D.Blank used "<account@domain>" in comments for his address. )  
    
    comment out IMPORT lines
    
    revise version

--- gramplet.gpr.py
+++ gramplet.gpr.py
@@ -21,11 +21,6 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
-from gramps.gen.plug._pluginreg import register, STABLE, BETA, EXPERIMENTAL, UNSTABLE, DEVELOPER, GRAMPLET
-from gramps.gen.const import GRAMPS_LOCALE as glocale
-_ = glocale.translation.gettext
-
-MODULE_VERSION = "5.2"
 
 # ------------------------------------------------------------------------
 #
@@ -33,8 +28,12 @@ MODULE_VERSION = "5.2"
 #
 # ------------------------------------------------------------------------
 
-from gramps.gen.const import GRAMPS_LOCALE as glocale
-_ = glocale.translation.sgettext
+# from gramps.gen.plug._pluginreg import register, STABLE, BETA, EXPERIMENTAL, UNSTABLE, DEVELOPER, GRAMPLET
+# from gramps.gen.const import GRAMPS_LOCALE as glocale
+# _ = glocale.translation.sgettext
+
+MODULE_VERSION = "5.2"
+
 
 # ------------------------------------------------------------------------
 #
@@ -50,13 +49,13 @@ register(GRAMPLET,
          gramps_target_version=MODULE_VERSION,
          status=STABLE,
          audience=EVERYONE,
-         maintainers="Brian McCullough",
-         maintainers_email="emyoulation@yahoo.com",
+         maintainers=["Nick Hall", "Brian McCullough"],
+         maintainers_email=["", "emyoulation@yahoo.com"],
          fname="Betatodo.py",
          height=200,
          gramplet='BetaPersonToDo',
          gramplet_title=_("gramplet|Beta Person To Do"),
-         version="0.1.0",
+         version="0.1.1",
          help_url="Gramps_5.1_Wiki_Manual_-_Gramplets#Person_To_Do",
          navtypes=["Person"],
          )
@@ -67,14 +66,14 @@ register(GRAMPLET,
          description=_("Beta Gramplet for displaying a To Do list"),
          status=EXPERIMENTAL,
          audience=EXPERT,
-         maintainers="Brian McCullough",
-         maintainers_email="emyoulation@yahoo.com",
+         maintainers=["Nick Hall","Brian McCullough"],
+         maintainers_email=["", "emyoulation@yahoo.com"],
          fname="Betatodogramplet.py",
          height=300,
          expand=True,
          gramplet='BetaToDoGramplet',
          gramplet_title=_("gramplet|Beta To Do"),
-         version="0.1.0",
+         version="0.1.1",
          help_url="Gramps_5.1_Wiki_Manual_-_Gramplets#To_Do",
          gramps_target_version=MODULE_VERSION,
          navtypes=["Dashboard"],
@@ -86,14 +85,14 @@ register(GRAMPLET,
          description=_("Beta Gramplet showing a welcome message"),
          status=BETA,
          audience=EVERYONE,
-         maintainers="Brian McCullough",
-         maintainers_email="emyoulation@yahoo.com",
+         maintainers=["Douglas S. Blank", "Dave Scheipers", "Brian McCullough"],
+         maintainers_email=["doug.blank@gmail.com", "", "emyoulation@yahoo.com"],
          fname="Betawelcomegramplet.py",
          height=300,
          expand=True,
          gramplet='BetaWelcomeGramplet',
          gramplet_title=_("Beta Welcome to Gramps!"),
-         version="0.1.0",
+         version="0.1.1",
          help_url="Gramps_5.1_Wiki_Manual_-_Gramplets#Welcome",
          gramps_target_version=MODULE_VERSION,
          )
@@ -104,14 +103,14 @@ register(GRAMPLET,
          description=_("Beta Gramplet suggesting items to research"),
          status=STABLE,
          audience=EVERYONE,
-         maintainers="Brian McCullough",
-         maintainers_email="emyoulation@yahoo.com",
+         maintainers=["Reinhard Mueller", "Jakim Friant", "Brian McCullough"],
+         maintainers_email=["", "jmodule@friant.org" ,"emyoulation@yahoo.com"],
          fname="Betawhatsnext.py",
          height=230,
          expand=True,
          gramplet='BetaWhatNextGramplet',
          gramplet_title=_("Beta What's Next?"),
-         version="0.1.1",
+         version="0.1.2",
          help_url="Gramps_5.1_Wiki_Manual_-_Gramplets#What\'s_Next",
          gramps_target_version=MODULE_VERSION,
          )
