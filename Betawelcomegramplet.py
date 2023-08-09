830c666d52ede0a106e158eede355e125616c547
Emyoulation or BAMaustin <69127217+emyoulation@users.noreply.github.com>
2023-08-08 22:10:30 -0500

    Update Betawelcomegramplet.py
    Changes for better GPLv2 compliance

--- Betawelcomegramplet.py
+++ Betawelcomegramplet.py
@@ -1,13 +1,13 @@
-# Gramps - a GTK+/GNOME based genealogy program
-#
-# Copyright (C) 2007-2009  Douglas S. Blank <doug.blank@gmail.com>
-# Copyright (C) 2020       Dave Scheipers
+# "Welcome to Gramps!" Gramplet, a modular plugin for Gramps 
+# (Gramps - the genealogy software suite built on GTK+/GNOME) 
+# 
+# Copyright (C) 2008 Reinhard Mueller
+# Copyright (C) 2010       Jakim Friant
 # Copyright (C) 2022       Brian McCullough
 #
 # This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+# it under the terms of version 2 of the GNU General Public License as 
+# published by the Free Software Foundation.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -17,6 +17,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# For the Gramps-Project volunteer community's contact information, visit:
+# https://gramps-project.org/wiki/index.php/Contact
+#
 
 # ------------------------------------------------------------------------
 #
