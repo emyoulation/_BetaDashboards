adb5099d1310b53616d3bd0d18cf45eead33f113
Emyoulation or BAMaustin <69127217+emyoulation@users.noreply.github.com>
2024-01-15 18:42:11 -0600

    Update gramplet.gpr.py
    Revise for 5.2 cross-compatibility

--- gramplet.gpr.py
+++ gramplet.gpr.py
@@ -5,7 +5,7 @@
 # Copyright (C) 2009 Benny Malengier
 # Copyright (C) 2011 Nick Hall
 # Copyright (C) 2011 Tim G L Lyons
-# Copyright (C) 2023 Brian McCullough
+# Copyright (C) 2024 Brian McCullough
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -32,7 +32,8 @@
 # from gramps.gen.const import GRAMPS_LOCALE as glocale
 # _ = glocale.translation.sgettext
 
-MODULE_VERSION = "5.2"
+from gramps.version import major_version
+MODULE_VERSION = major_version
 
 
 # ------------------------------------------------------------------------
@@ -48,15 +49,12 @@ register(GRAMPLET,
          description=_("Beta Gramplet showing the To Do notes for a person"),
          gramps_target_version=MODULE_VERSION,
          status=STABLE,
-         audience=EVERYONE,
-         maintainers=["Nick Hall", "Brian McCullough"],
-         maintainers_email=["", "emyoulation@yahoo.com"],
          fname="Betatodo.py",
          height=200,
          gramplet='BetaPersonToDo',
-         gramplet_title=_("gramplet|Beta Person To Do"),
+         gramplet_title=_("β Person To Do"),
          version="0.1.1",
-         help_url="Gramps_5.1_Wiki_Manual_-_Gramplets#Person_To_Do",
+         help_url="Gramps_5.2_Wiki_Manual_-_Gramplets#Person_To_Do",
          navtypes=["Person"],
          )
 
@@ -64,17 +62,14 @@ register(GRAMPLET,
          id="Beta To Do",
          name=_("Beta To Do"),
          description=_("Beta Gramplet for displaying a To Do list"),
-         status=EXPERIMENTAL,
-         audience=EXPERT,
-         maintainers=["Nick Hall","Brian McCullough"],
-         maintainers_email=["", "emyoulation@yahoo.com"],
+         status=STABLE,
          fname="Betatodogramplet.py",
          height=300,
          expand=True,
          gramplet='BetaToDoGramplet',
-         gramplet_title=_("gramplet|Beta To Do"),
+         gramplet_title=_("β Dashboard To Do"),
          version="0.1.1",
-         help_url="Gramps_5.1_Wiki_Manual_-_Gramplets#To_Do",
+         help_url="Gramps_5.2_Wiki_Manual_-_Gramplets#To_Do",
          gramps_target_version=MODULE_VERSION,
          navtypes=["Dashboard"],
          )
@@ -83,17 +78,14 @@ register(GRAMPLET,
          id="Beta Welcome",
          name=_("Beta Welcome"),
          description=_("Beta Gramplet showing a welcome message"),
-         status=BETA,
-         audience=EVERYONE,
-         maintainers=["Douglas S. Blank", "Dave Scheipers", "Brian McCullough"],
-         maintainers_email=["doug.blank@gmail.com", "", "emyoulation@yahoo.com"],
+         status=STABLE,
          fname="Betawelcomegramplet.py",
          height=300,
          expand=True,
          gramplet='BetaWelcomeGramplet',
-         gramplet_title=_("Beta Welcome to Gramps!"),
+         gramplet_title=_("β Welcome to Gramps!"),
          version="0.1.1",
-         help_url="Gramps_5.1_Wiki_Manual_-_Gramplets#Welcome",
+         help_url="Gramps_5.2_Wiki_Manual_-_Gramplets#Welcome",
          gramps_target_version=MODULE_VERSION,
          )
 
@@ -102,14 +94,11 @@ register(GRAMPLET,
          name=_("Beta What's Next"),
          description=_("Beta Gramplet suggesting items to research"),
          status=STABLE,
-         audience=EVERYONE,
-         maintainers=["Reinhard Mueller", "Jakim Friant", "Brian McCullough"],
-         maintainers_email=["", "jmodule@friant.org" ,"emyoulation@yahoo.com"],
          fname="Betawhatsnext.py",
          height=230,
          expand=True,
          gramplet='BetaWhatNextGramplet',
-         gramplet_title=_("Beta What's Next?"),
+         gramplet_title=_("β What's Next?"),
          version="0.1.3",
          help_url="https://github.com/emyoulation/_BetaDashboards/blob/5.2/README.md",
          gramps_target_version=MODULE_VERSION,
